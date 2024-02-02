ec3c0bdc157d09b21d512679ee83a8d88ea235dd
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:15:25 +0500

    UpdateFoodCatDto

---
+++ src/StudyCenter.Domain/Dtos/Food_Category/Food_CatUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace StudyCenter.Domain.Dtos.Food_Category
+{
+    public class Food_CatUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
