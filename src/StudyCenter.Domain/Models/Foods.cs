212ff11aeada8f624a96feae83543726f9ef948c
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:16:44 +0500

    Foods Model Added

---
+++ src/StudyCenter.Domain/Models/Foods.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyCenter.Domain.Models
+{
+    public class Foods
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public decimal Food_Rate {  get; set; }
+        public int FoodCategory_id {  get; set; }
+
+        public Food_Category Food_Category { get; set; }
+    }
+}
