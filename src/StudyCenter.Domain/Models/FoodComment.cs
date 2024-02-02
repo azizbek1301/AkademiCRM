de98645dd0d259059477fe5259b2de15aa4bf380
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:16:33 +0500

    FoodComment Model

---
+++ src/StudyCenter.Domain/Models/FoodComment.cs
@@ -0,0 +1,15 @@
+﻿namespace StudyCenter.Domain.Models
+{
+    public class FoodComment
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+        public int Food_id { get; set; }
+
+        public Foods Foods { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+        public ICollection<Teacher> Teachers { get; set; }
+    }
+}
