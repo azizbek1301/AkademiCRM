32ef70a7b5e1cad84d25220fa1bd42df0216545f
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:16:21 +0500

    Event Model

---
+++ src/StudyCenter.Domain/Models/Events.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyCenter.Domain.Models
+{
+    public class Events
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Subject_id { get; set; }
+
+        public Subject Subject { get; set; }
+    }
+}
