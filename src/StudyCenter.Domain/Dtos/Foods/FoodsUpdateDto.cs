98ed12c981fcb9ef1bbbec7891ab670357fc728d
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:14:49 +0500

    UpdateFoodDto

---
+++ src/StudyCenter.Domain/Dtos/Foods/FoodsUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCenter.Domain.Dtos.Foods
+{
+    public class FoodsUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Food_Rate { get; set; }
+        public int FoodCategory_id { get; set; }
+    }
+}
