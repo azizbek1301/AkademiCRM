57040923582d648f3820c472728f4bd9a5056522
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 11:16:03 +0500

    StudentCreateDto

---
+++ src/StudyCenter.Domain/Dtos/Student/StudentCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace StudyCenter.Domain.Dtos.Student
+{
+    public class StudentCreateDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; } = default!;
+        public string ParentName { get; set; }
+        public decimal Grade { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public IFormFile ImageUrl { get; set; }
+    }
+}
