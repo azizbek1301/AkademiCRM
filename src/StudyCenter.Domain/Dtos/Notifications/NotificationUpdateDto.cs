2f9d31ef94e9c518264f0c0c6ffaa551f77e61e7
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:15:58 +0500

    UpdateNotificationDto

---
+++ src/StudyCenter.Domain/Dtos/Notifications/NotificationUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyCenter.Domain.Dtos.Notifications
+{
+    public class NotificationUpdateDto
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public int Student_Id { get; set; }
+    }
+}
