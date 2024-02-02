73002a75ae9893527982b3f99b955436b103982c
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:13:21 +0500

    UpdateAdmin Dto

---
+++ src/StudyCenter.Domain/Dtos/Admins/UpdateAdminDto.cs
@@ -0,0 +1,13 @@
+﻿using StudyCenter.Domain.Enums;
+
+namespace StudyCenter.Domain.Dtos.Admins
+{
+    public class UpdateAdminDto
+    {
+        public int AdminId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
