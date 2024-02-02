f38506b59010d99254f9a32f163f83bd09073ffe
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:13:49 +0500

    UpdateEventDto

---
+++ src/StudyCenter.Domain/Dtos/Events/EventUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyCenter.Domain.Dtos.Events
+{
+    public class EventUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Subject_id { get; set; }
+    }
+}
