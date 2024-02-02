72fa1409464b690042140473825aa4fe436f73a2
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:13:38 +0500

    CretaeEventDto

---
+++ src/StudyCenter.Domain/Dtos/Events/EventCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StudyCenter.Domain.Dtos.Events
+{
+    public class EventCreateDto
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Subject_id { get; set; }
+    }
+}
