29135c2f0864013e904a1502790d6f793f2aa7cd
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:13:10 +0500

    create Admin Dto

---
+++ src/StudyCenter.Domain/Dtos/Admins/CreateAdminDto.cs
@@ -0,0 +1,18 @@
+﻿using StudyCenter.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCenter.Domain.Dtos.Admins
+{
+    public class CreateAdminDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
