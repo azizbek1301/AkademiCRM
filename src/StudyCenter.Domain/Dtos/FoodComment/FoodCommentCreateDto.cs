663bd56db778358e15a07c3375f95c954a114ba2
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-02 10:14:04 +0500

    CreateFoodCommentDto

---
+++ src/StudyCenter.Domain/Dtos/FoodComment/FoodCommentCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StudyCenter.Domain.Dtos.FoodComment
+{
+    public class FoodCommentCreateDto
+    {
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+        public int Food_id { get; set; }
+    }
+}
