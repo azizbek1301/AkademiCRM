beec9a6774dfcbe2ab6018b661b1828fa6cb3257
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:37:40 +0500

    GetEventByIdQuery

---
+++ src/StudyCenter.Service/UseCases/Event/Queries/GetEventByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Event.Queries
+{
+    public class GetEventByIdQuery:IRequest<Events>
+    {
+        public int Id {  get; set; }
+
+    }
+}
