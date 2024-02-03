dfd718bfab51b8dc37cfc73752e65089ba7a03aa
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:33:03 +0500

    GetEventByIdHandler

---
+++ src/StudyCenter.Service/UseCases/Event/Handler/GetEventByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using StudyCenter.Domain.Models;
+using StudyCenter.Service.Abstraction.DataAccess;
+using StudyCenter.Service.UseCases.Event.Queries;
+
+namespace StudyCenter.Service.UseCases.Event.Handler
+{
+    public class GetEventByIdHandler : IRequestHandler<GetEventByIdQuery, Events>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetEventByIdHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Events> Handle(GetEventByIdQuery request, CancellationToken cancellationToken)
+        {
+            Events? events = await _context.Event.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+            if (events == null)
+            {
+                throw new Exception("Document Not Found");
+            }
+            return events;
+        }
+    }
+}
