8d71ccfa9427977b0bbb388bdca93211e96d6a91
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:13:48 +0500

    Create Food Dto

--- src/StudyCenter.Domain/Dtos/Foods/FoodsCraeteDto.cs
+++ src/StudyCenter.Domain/Dtos/Foods/FoodsCraeteDto.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudyCenter.Domain.Dtos.Foods
+﻿namespace StudyCenter.Domain.Dtos.Foods
 {
     public class FoodsCraeteDto
     {
-        
+
         public string Name { get; set; }
         public decimal Food_Rate { get; set; }
         public int FoodCategory_id { get; set; }
