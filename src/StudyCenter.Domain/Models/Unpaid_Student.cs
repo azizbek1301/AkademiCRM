282708591e42de7b7c9d8e583a6bf1f3ac1f0b17
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:20:59 +0500

    Unpaid Student Model

---
+++ src/StudyCenter.Domain/Models/Unpaid_Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyCenter.Domain.Models
+{
+    public class Unpaid_Student
+    {
+        public int Id { get; set; }
+        public int StudentId { get;set; }
+
+        public decimal Price { get; set; }
+
+        public Student Student { get; set; }
+    }
+}
