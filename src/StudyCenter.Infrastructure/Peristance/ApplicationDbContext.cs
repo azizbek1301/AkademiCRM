cf3de2a20b930d6e8818de5d29198f435ef27510
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:22:32 +0500

    ApplicationDbContext

---
+++ src/StudyCenter.Infrastructure/Peristance/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyCenter.Domain.Models;
+using StudyCenter.Service.Abstraction.DataAccess;
+
+namespace StudyCenter.Infrastructure.Peristance
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+            Database.Migrate();
+        }
+        
+        public DbSet<Events> Event { get; set; }
+        public DbSet<Food_Category> Food_Categories { get; set; }
+        public DbSet<FoodComment> Food_Comments { get; set; }
+        public DbSet<Foods> Foods { get; set; }
+        public DbSet<Notifications> Notifications { get; set; }
+        public DbSet<School_Expence> School_Expences { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Teacher_Education> Teacher_Educations { get; set; }
+        public DbSet<TeacherClass> TeacherClasses { get; set; }
+        public DbSet<Unpaid_Student> Unpaid_Students { get; set; }
+    }
+}
