359b9054c14434edcb82423fa6c4400c7c5b70b4
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:26:00 +0500

    Media Helper

---
+++ src/StudyCenter.Service/Common/Helpers/MediaHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace StudyCenter.Service.Common.Helpers
+{
+    public class MediaHelper
+    {
+        public static string MakeImageName(string fileName)
+        {
+            var fileInfo = new FileInfo(fileName);
+            var imageExtensions = GetImageExtensions();
+
+            if (imageExtensions.Any(x => x == fileInfo.Extension))
+            {
+                string extensions = fileInfo.Extension;
+                string name = "IMG_" + Guid.NewGuid() + extensions;
+                return name;
+            }
+            throw new Exception("Image Not Found");
+
+        }
+
+
+
+        public static string[] GetImageExtensions()
+        {
+            return new string[]
+            {
+                ".jpg",
+                ".jpeg",
+                ".png",
+                ".bmp",
+                ".svg"
+            };
+        }
+    }
+}
