f6f2e0ab60adcb1f6200f263fadaaecc60991920
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:20:23 +0500

    TeacherClass Model

---
+++ src/StudyCenter.Domain/Models/TeacherClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCenter.Domain.Models
+{
+    public class TeacherClass
+    {
+        public int Id { get; set; }
+        public int Teacher_id {  get; set; }
+        public int Subject_id { get; set; }
+        public int Student_id {  get; set; }
+
+        public Teacher Teacher { get; set; }
+        public Subject Subject { get; set; }
+        public Student Student { get; set; }
+
+    }
+}
