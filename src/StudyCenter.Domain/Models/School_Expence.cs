e6174896540de20eaf4e9dd65f51c14096be1fa6
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:19:43 +0500

    School_Expence model

---
+++ src/StudyCenter.Domain/Models/School_Expence.cs
@@ -0,0 +1,13 @@
+﻿using StudyCenter.Domain.Enums;
+
+namespace StudyCenter.Domain.Models
+{
+    public class School_Expence
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ExpenceDate {  get; set; }
+        public decimal Price {  get; set; }
+        public Status Status { get; set; }
+    }
+}
