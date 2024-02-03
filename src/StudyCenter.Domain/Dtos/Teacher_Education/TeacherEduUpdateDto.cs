24c43f8f38240d1c29d2641f0847c3bc20d08afc
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:16:24 +0500

    Teacher Edu Update Dto

---
+++ src/StudyCenter.Domain/Dtos/Teacher_Education/TeacherEduUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCenter.Domain.Dtos.Teacher_Education
+{
+    internal class TeacherEduUpdateDto
+    {
+    }
+}
