410259d9b4e9382de2fc799a1b9b7ebc9de951bf
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:37:28 +0500

    GetAllEventQuery

---
+++ src/StudyCenter.Service/UseCases/Event/Queries/GetAllEventQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Event.Queries
+{
+    public class GetAllEventQuery:IRequest<IEnumerable<Events>>
+    {
+    }
+}
