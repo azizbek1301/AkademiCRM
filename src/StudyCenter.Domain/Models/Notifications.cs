d03b1c77698f2d06fde0b2aa0fd5db769e8ded79
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:17:52 +0500

    notifications model

---
+++ src/StudyCenter.Domain/Models/Notifications.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyCenter.Domain.Models
+{
+    public class Notifications
+    {
+        public int Id { get; set; }
+        public string MEssage { get; set; }
+        public DateTime Date { get; set; }
+        public int Student_Id { get; set; }
+
+        public Student Student { get; set; }
+    }
+}
