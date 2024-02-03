a2729a7d821f6bcd0e0c45ccd740fc1ffa3b8674
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:17:40 +0500

    Food_Category Model

---
+++ src/StudyCenter.Domain/Models/Food_Category.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyCenter.Domain.Models
+{
+    public class Food_Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Foods> Foods { get; set; }
+    }
+}
