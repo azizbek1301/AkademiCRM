d9c93e946c83f885eb0a4bb28fdec93b42ea4283
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:26:59 +0500

    CreateEventHandler

---
+++ src/StudyCenter.Service/UseCases/Event/Handler/CreateEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+using StudyCenter.Service.Abstraction.DataAccess;
+using StudyCenter.Service.UseCases.Event.Command;
+
+namespace StudyCenter.Service.UseCases.Event.Handler
+{
+    public class CreateDocumentHandler : IRequestHandler<CreateEventCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateDocumentHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateEventCommand request, CancellationToken cancellationToken)
+        {
+            Events events = new Events()
+            {
+                Name = request.Name,
+                Date = request.Date,
+                Subject_id = request.Subject_id,
+            };
+
+            await _context.Event.AddAsync(events, cancellationToken);
+            int responce= await _context.SaveChangesAsync(cancellationToken);
+            return responce;
+        }
+    }
+}
