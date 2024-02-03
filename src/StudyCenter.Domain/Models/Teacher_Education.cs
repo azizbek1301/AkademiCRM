8ab4635301203fe299fc9e82655e0244660fcb40
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:20:46 +0500

    Teacher Education model

---
+++ src/StudyCenter.Domain/Models/Teacher_Education.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyCenter.Domain.Models
+{
+    public class Teacher_Education
+    {
+        public int Id { get; set; }
+        public string EducationName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public ICollection<Teacher> Teachers { get; set; }
+    }
+}
