da314c8fcc3cda5620f4c209b946808c8a434b77
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:20:03 +0500

    Subject model

---
+++ src/StudyCenter.Domain/Models/Subject.cs
@@ -0,0 +1,11 @@
+﻿namespace StudyCenter.Domain.Models
+{
+    public class Subject
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Events> Events { get; set; }
+        public ICollection<TeacherClass> TeacherClassess { get; set; }
+    }
+}
