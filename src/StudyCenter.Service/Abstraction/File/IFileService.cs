be82591fb73357114f638f0577ad5b00b0077dfd
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:25:50 +0500

    IFileService

---
+++ src/StudyCenter.Service/Abstraction/File/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace StudyCenter.Service.Abstraction.File
+{
+    public interface IFileService
+    {
+        ValueTask<string> UploadImageAsync(IFormFile file);
+        ValueTask<bool> DeleteImageAsync(string file);
+        ValueTask<byte[]> GetImageAsync(string path);
+
+    }
+}
