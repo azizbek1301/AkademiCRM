76caa717234c34ae513bc223e09675c2592a3586
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:26:44 +0500

    UpdateEventCommand

---
+++ src/StudyCenter.Service/UseCases/Event/Command/UpdateEventCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Dtos.Events;
+
+namespace StudyCenter.Service.UseCases.Event.Command
+{
+    public class UpdateEventCommand:EventUpdateDto,IRequest<int>
+    {
+    }
+}
