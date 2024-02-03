8f6d912fe20019da5aa6095baba807e55b77d8cc
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-03 22:23:27 +0500

    Dependency injection

---
+++ src/StudyCenter.Service/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using StudyCenter.Service.Abstraction.File;
+using StudyCenter.Service.Service.Files;
+using System.Reflection;
+
+namespace StudyCenter.Service
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient<IFileService, FileService>();
+            return services;
+        }
+    }
+}
