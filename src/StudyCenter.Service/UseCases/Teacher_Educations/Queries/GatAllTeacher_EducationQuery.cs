1f68a7457e3cca45fd4e974e2f7963eefde2e8a6
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:13:18 +0500

    Teacher Education Query

---
+++ src/StudyCenter.Service/UseCases/Teacher_Educations/Queries/GatAllTeacher_EducationQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Teacher_Educations.Queries
+{
+    public class GatAllTeacher_EducationQuery : IRequest<List<Teacher_Education>>
+    {
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Teacher_Educations/Queries/GetTeacher_EducationByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Teacher_Educations.Queries
+{
+    public class GetTeacher_EducationByIdQuery : IRequest<Teacher_Education>
+    {
+        public int Id { get; set; }
+    }
+}
