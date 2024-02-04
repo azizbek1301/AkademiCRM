1b01e9ec19cc92af50ad8eda00ab8054b9d6ddcc
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:13:48 +0500

    Unpaid Student Query

---
+++ src/StudyCenter.Service/UseCases/Unpaid_Students/Queries/GetAllUnpaidStudentQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Unpaid_Students.Queries
+{
+    public class GetAllUnpaidStudentQuery : IRequest<List<Unpaid_Student>>
+    {
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Unpaid_Students/Queries/GetUnpaidStudentByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCenter.Service.UseCases.Unpaid_Students.Queries
+{
+    public class GetUnpaidStudentByIdQuery:IRequest<Unpaid_Student>
+    {
+        public int Id { get; set; } 
+    }
+}
