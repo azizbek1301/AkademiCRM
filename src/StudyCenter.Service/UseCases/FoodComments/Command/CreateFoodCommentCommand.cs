6b0645690e87d827669201ce6d7b118c7cebd07a
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:03:20 +0500

    FoodComment command

---
+++ src/StudyCenter.Service/UseCases/FoodComments/Command/CreateFoodCommentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Dtos.FoodComment;
+
+namespace StudyCenter.Service.UseCases.FoodComments.Command
+{
+    public class CreateFoodCommentCommand:FoodCommentCreateDto,IRequest<int>
+    {
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/FoodComments/Command/DeleteFoodCommentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace StudyCenter.Service.UseCases.FoodComments.Command
+{
+    public class DeleteFoodCommentCommand:IRequest<int>
+    {
+        public int Id { get; set; } 
+    }
+}
