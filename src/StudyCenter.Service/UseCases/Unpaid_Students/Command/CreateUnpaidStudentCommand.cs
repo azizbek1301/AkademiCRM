316e9a133e756f87def70eb6fef2bd001c9270ba
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:13:28 +0500

    Unpaid Student command

---
+++ src/StudyCenter.Service/UseCases/Unpaid_Students/Command/CreateUnpaidStudentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Dtos.Unpaid_Student;
+
+namespace StudyCenter.Service.UseCases.Unpaid_Students.Command
+{
+    public class CreateUnpaidStudentCommand : UnpaidStudentCreateDto, IRequest<int>
+    {
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Unpaid_Students/Command/DeleteunpaidStudentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace StudyCenter.Service.UseCases.Unpaid_Students.Command
+{
+    public class DeleteunpaidStudentCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+    }
+}
