57bbfa140cdfa00c1f5412a8fa7dfd3e4e42eaef
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:06:46 +0500

    School Expence Query

---
+++ src/StudyCenter.Service/UseCases/School_Expences/Queries/GetAllSchool_ExtenceQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.School_Expences.Queries
+{
+    public class GetAllSchool_ExtenceQuery:IRequest<List<School_Expence>>
+    {
+    }
+}
