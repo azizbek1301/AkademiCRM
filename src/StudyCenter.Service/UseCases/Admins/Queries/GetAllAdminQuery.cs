86fafad2c18192702d479602e4cb857a1a65a2ab
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:48:44 +0500

    Admin Queries

---
+++ src/StudyCenter.Service/UseCases/Admins/Queries/GetAllAdminQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Admins.Queries
+{
+    public class GetAllAdminQuery : IRequest<List<Admin>>
+    {
+    }
+}
