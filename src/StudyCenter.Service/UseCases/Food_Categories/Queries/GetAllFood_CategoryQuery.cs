d0fb047ebd4ead8750855ae22938bb418f446dc0
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:05:07 +0500

    Food-Category Query

---
+++ src/StudyCenter.Service/UseCases/Food_Categories/Queries/GetAllFood_CategoryQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Food_Categories.Queries
+{
+    public class GetAllFood_CategoryQuery : IRequest<List<Food_Category>>
+    {
+
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Food_Categories/Queries/GetFood_CategoryByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Food_Categories.Queries
+{
+    public class GetFood_CategoryByIdQuery:IRequest<Food_Category>
+    {
+        public int Id { get; set; } 
+    }
+}
