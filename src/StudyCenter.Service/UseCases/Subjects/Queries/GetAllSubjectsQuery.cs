9e735589e3d52abd781d09cd95dfcd5f4eae001a
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:11:31 +0500

    Subject Queries

---
+++ src/StudyCenter.Service/UseCases/Subjects/Queries/GetAllSubjectsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Subjects.Queries
+{
+    public class GetAllSubjectsQuery:IRequest<List<Subject>>
+    {
+    }
+}
