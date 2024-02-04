620189bfa4e708b60b1bbf70e060a923e39a6b03
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:12:41 +0500

    Teacher Query

---
+++ src/StudyCenter.Service/UseCases/Teachers/Queries/GetAllTeacherQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Teachers.Queries
+{
+    public class GetAllTeacherQuery : IRequest<List<Teacher>>
+    {
+
+    }
+}
