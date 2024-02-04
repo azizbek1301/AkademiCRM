16720e48c03512da5404cacd9c5149b80b7a211c
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:10:24 +0500

    Student Command

---
+++ src/StudyCenter.Service/UseCases/Students/Command/CreateStudentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Dtos.Student;
+
+namespace StudyCenter.Service.UseCases.Students.Command
+{
+    public class CreateStudentCommand : StudentCreateDto, IRequest<int>
+    {
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Students/Command/DeleteStudentsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace StudyCenter.Service.UseCases.Students.Command
+{
+    public class DeleteStudentsCommand:IRequest<int>
+    {
+        public int Id { get; set; }     
+    }
+}
