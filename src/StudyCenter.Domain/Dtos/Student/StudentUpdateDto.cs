6a2f5ac3f9dfd7f805bcd1f4cddb5de980a422ce
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:45:59 +0500

    StudentDto

--- src/StudyCenter.Domain/Dtos/Student/StudentUpdateDto.cs
+++ src/StudyCenter.Domain/Dtos/Student/StudentUpdateDto.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 
 namespace StudyCenter.Domain.Dtos.Student
 {
-    internal class StudentUpdateDto
+    public class StudentUpdateDto
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string ParentName { get; set; }
+        public decimal Grade { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public IFormFile ImageUrl { get; set; }
     }
 }
