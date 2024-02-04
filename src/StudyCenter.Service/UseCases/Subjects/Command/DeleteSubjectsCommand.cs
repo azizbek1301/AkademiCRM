b2b25f8b702568f92a347e47facd6284e0b774ac
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:11:11 +0500

    Subject Command

---
+++ src/StudyCenter.Service/UseCases/Subjects/Command/DeleteSubjectsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace StudyCenter.Service.UseCases.Subjects.Command
+{
+    public class DeleteSubjectsCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Subjects/Command/UpdateSubjectsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Dtos.Subject;
+
+namespace StudyCenter.Service.UseCases.Subjects.Command
+{
+    public class UpdateSubjectsCommand : SubjectUpdateDto, IRequest<int>
+    {
+    }
+}
