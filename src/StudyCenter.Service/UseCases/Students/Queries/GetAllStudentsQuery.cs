0fc5624b3c5a121bf19a8ff503db04b9e7b93652
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:10:47 +0500

    Student Query

---
+++ src/StudyCenter.Service/UseCases/Students/Queries/GetAllStudentsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Students.Queries
+{
+    public class GetAllStudentsQuery:IRequest<List<Student>>
+    {
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Students/Queries/GetStudentsByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCenter.Service.UseCases.Students.Queries
+{
+    public class GetStudentsByIdQuery:IRequest<Student>
+    {
+        public int Id {  get; set; }    
+    }
+}
