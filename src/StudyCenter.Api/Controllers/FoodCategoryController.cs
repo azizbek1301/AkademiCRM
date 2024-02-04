19dd07aa7efb9d4278b8d5ec860ac686d60dff0b
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:39:22 +0500

    FoodCategoryController

---
+++ src/StudyCenter.Api/Controllers/FoodCategoryController.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudyCenter.Service.UseCases.Food_Categories.Command;
+using StudyCenter.Service.UseCases.Food_Categories.Queries;
+
+namespace StudyCenter.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FoodCategoryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FoodCategoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(CreateFood_CategoryCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+        [Authorize(Roles = "SuperAdmin")]
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var response = await _mediator.Send(new GetAllFood_CategoryQuery());
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateAsync(UpdateFood_CategoryCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(int id)
+        {
+            int response = await _mediator.Send(new DeleteFood_CategoryCommand() { Id = id });
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdAsync(int id)
+        {
+            var response = await _mediator.Send(new GetFood_CategoryByIdQuery() { Id = id });
+
+            return Ok(response);
+        }
+    }
+}
