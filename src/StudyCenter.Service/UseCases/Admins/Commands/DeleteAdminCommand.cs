7f000b99bbff9e8b133f26c3e3dcf7e511058a31
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:48:22 +0500

    Admin Commands

---
+++ src/StudyCenter.Service/UseCases/Admins/Commands/DeleteAdminCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace StudyCenter.Service.UseCases.Admins.Commands
+{
+    public class DeleteAdminCommand:IRequest<int>
+    {
+        public int AdminId {  get; set; }
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Admins/Commands/UpdateAdminCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Dtos.Admins;
+
+namespace StudyCenter.Service.UseCases.Admins.Commands
+{
+    public class UpdateAdminCommand : UpdateAdminDto, IRequest<int>
+    {
+    }
+}
