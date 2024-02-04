091564ef6068e827914bd8c0cec4011657ecaf60
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:12:08 +0500

    Teacher Class Query

---
+++ src/StudyCenter.Service/UseCases/TeacherClasses/Queries/GetAllTeacherClassesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.TeacherClasses.Queries
+{
+    public class GetAllTeacherClassesQuery:IRequest<List<TeacherClass>>
+    {
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/TeacherClasses/Queries/GetTeacherClassesByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.TeacherClasses.Queries
+{
+    public class GetTeacherClassesByIdQuery : IRequest<TeacherClass>
+    {
+        public int Id { get; set; }
+    }
+}
