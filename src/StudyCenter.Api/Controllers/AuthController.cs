c19a981e9797f7718e43b7b3558bea2ade367623
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:38:59 +0500

    AuthController

---
+++ src/StudyCenter.Api/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Authorizatsiya.Api.Interface;
+using Authorizatsiya.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyCenter.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> LoginAsync(Login login)
+        {
+            string token = await _authService.GenerateToken(login);
+
+            return Ok(token);
+        }
+    }
+}
