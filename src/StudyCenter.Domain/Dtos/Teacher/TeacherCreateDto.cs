8790459c9702a7649a02aabb0e8f795942475962
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:46:16 +0500

    TeacherDto

--- src/StudyCenter.Domain/Dtos/Teacher/TeacherCreateDto.cs
+++ src/StudyCenter.Domain/Dtos/Teacher/TeacherCreateDto.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudyCenter.Domain.Dtos.Teacher
+﻿namespace StudyCenter.Domain.Dtos.Teacher
 {
-    internal class TeacherCreateDto
+    public class TeacherCreateDto
     {
+
+        public string FullName { get; set; }
+        public string SubjectName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string About { get; set; }
+        public int Education_id { get; set; }
     }
 }
--- src/StudyCenter.Domain/Dtos/Teacher/TeacherUpdateDto.cs
+++ src/StudyCenter.Domain/Dtos/Teacher/TeacherUpdateDto.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace StudyCenter.Domain.Dtos.Teacher
 {
-    internal class TeacherUpdateDto
+    public class TeacherUpdateDto
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string SubjectName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string About { get; set; }
+        public int Education_id { get; set; }
     }
 }
