27c8d64a38892c77a3a046119a33a5e423603f8d
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:39:34 +0500

    FoodCommentController

---
+++ src/StudyCenter.Api/Controllers/FoodCommentController.cs
@@ -0,0 +1,62 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudyCenter.Service.UseCases.Event.Command;
+using StudyCenter.Service.UseCases.Event.Queries;
+using StudyCenter.Service.UseCases.FoodComments.Command;
+using StudyCenter.Service.UseCases.FoodComments.Queries;
+
+namespace StudyCenter.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FoodCommentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FoodCommentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(CreateFoodCommentCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+        [Authorize(Roles = "SuperAdmin")]
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var response = await _mediator.Send(new GetAllFoodCommentQuery());
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateAsync(UpdateFoodCommentCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(int id)
+        {
+            int response = await _mediator.Send(new DeleteFoodCommentCommand() { Id = id });
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdAsync(int id)
+        {
+            var response = await _mediator.Send(new GetFoodCommentByIdQuery() { Id = id });
+
+            return Ok(response);
+        }
+    }
+}
