1e19187c8c09870ac59e9469eb56bd32af11572b
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:39:10 +0500

    EventsController

---
+++ src/StudyCenter.Api/Controllers/EventsController.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudyCenter.Service.UseCases.Event.Command;
+using StudyCenter.Service.UseCases.Event.Queries;
+
+namespace StudyCenter.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public EventsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync( CreateEventCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+        [Authorize(Roles ="SuperAdmin")]
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var response = await _mediator.Send(new GetAllEventQuery());
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateAsync(UpdateEventCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(int id)
+        {
+            int response = await _mediator.Send(new DeleteEvenetCommand() { Id = id });
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdAsync(int id)
+        {
+            var response = await _mediator.Send(new GetEventByIdQuery() { Id = id });
+
+            return Ok(response);
+        }
+    }
+}
