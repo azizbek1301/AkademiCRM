9c07a27810b16fc4e40df5e5d98577d913c6899e
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:47:16 +0500

    Admin Model

---
+++ src/StudyCenter.Domain/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using StudyCenter.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudyCenter.Domain.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public Role Role { get; set; }
+    }
+}
