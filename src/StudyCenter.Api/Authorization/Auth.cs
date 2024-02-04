2dab79922cd289c3d76aaf19a4f79199f92c8b7b
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:38:26 +0500

    Authorization

--- src/StudyCenter.Api/Authorization/Auth.cs
+++ src/StudyCenter.Api/Authorization/Auth.cs
@@ -1,6 +1,61 @@
-﻿namespace StudyCenter.Api.Authorization
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace StudyCenter.Api.Authorization
 {
-    public class Auth
+    public static class Auth
     {
+        public static IServiceCollection AddAutorization(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = configuration["JWT:ValidIssuer"],
+                        ValidateAudience = true,
+                        ValidAudience = configuration["JWT:ValidAudience"],
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+                    };
+                });
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "AuthDemo",
+                    Description = "Auth Demo Description"
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = "Bearer Authentication",
+                    Type = SecuritySchemeType.Http
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string> ()
+                    }
+
+                });
+            });
+
+            return services;
+        }
     }
 }
