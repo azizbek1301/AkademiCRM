d35fd9c3580e6ee9adcb0b1d8a08b9d4a78fccc0
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:44:06 +0500

    UnpaidStudentController

---
+++ src/StudyCenter.Api/Controllers/UnpaidStudentController.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudyCenter.Service.UseCases.Event.Command;
+using StudyCenter.Service.UseCases.Event.Queries;
+using StudyCenter.Service.UseCases.Unpaid_Students.Command;
+using StudyCenter.Service.UseCases.Unpaid_Students.Queries;
+
+namespace StudyCenter.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UnpaidStudentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UnpaidStudentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(CreateUnpaidStudentCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var response = await _mediator.Send(new GetAllUnpaidStudentQuery());
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateAsync(UpdateUnpaidStudentCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(int id)
+        {
+            int response = await _mediator.Send(new DeleteunpaidStudentCommand() { Id = id });
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdAsync(int id)
+        {
+            var response = await _mediator.Send(new GetUnpaidStudentByIdQuery() { Id = id });
+
+            return Ok(response);
+        }
+    }
+}
