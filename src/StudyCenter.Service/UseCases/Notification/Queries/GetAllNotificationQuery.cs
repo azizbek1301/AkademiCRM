b6237b440bec0863790482126119528dbc5a19cc
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 21:05:51 +0500

    Notification query

---
+++ src/StudyCenter.Service/UseCases/Notification/Queries/GetAllNotificationQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Notification.Queries
+{
+    public class GetAllNotificationQuery:IRequest<List<Notifications>>
+    {
+    }
+}
---
+++ src/StudyCenter.Service/UseCases/Notification/Queries/GetNotificationByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using StudyCenter.Domain.Models;
+
+namespace StudyCenter.Service.UseCases.Notification.Queries
+{
+    public class GetNotificationByIdQuery:IRequest<Notifications>
+    {
+        public int Id { get; set; } 
+    }
+}
