b93cd8ec2bb9b0be659f866668d46b48834702ea
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-02-04 20:40:37 +0500

    StudentController

---
+++ src/StudyCenter.Api/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudyCenter.Service.UseCases.Event.Command;
+using StudyCenter.Service.UseCases.Event.Queries;
+using StudyCenter.Service.UseCases.Students.Command;
+using StudyCenter.Service.UseCases.Students.Queries;
+
+namespace StudyCenter.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync([FromForm]CreateStudentCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var response = await _mediator.Send(new GetAllStudentsQuery());
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateAsync([FromForm]UpdateStudentsCommand command)
+        {
+            int response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(int id)
+        {
+            int response = await _mediator.Send(new DeleteStudentsCommand() { Id = id });
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdAsync(int id)
+        {
+            var response = await _mediator.Send(new GetStudentsByIdQuery() { Id = id });
+
+            return Ok(response);
+        }
+    }
+}
